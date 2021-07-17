d2715a3d4987d494bb0d05eb429aa22afe3237ca
Neev-coding <77157273+Neev-coding@users.noreply.github.com>
2021-07-17 18:26:45 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,85 @@
+canvas=document.getElementById("myCanvas");
+ctx=canvas.getContext("2d")
+
+img_width = 300;
+img_height = 100;
+
+var img_image;
+
+img_x = 100;
+img_y = 100;
+
+function add() {
+	img_imgTag = new Image(); 
+	img_imgTag.onload = uploadimg; 
+	img_imgTag.src = img_image;   
+
+function uploadimg() {
+
+	ctx.drawImage(img_imgTag, img_x, img_y, img_width, img_height);
+}
+
+window.addEventListener("keydown".my_keydown);
+function my_keydown(e)
+{
+	keyPressed = e.keyCode;
+	console.log(keyPressed);
+	
+		if((keyPressed >=97 && keyPressed<=122)|| (keyPressed >=65 && keyPressed<=90)){
+			alphabetkey();
+			document.getElementById("d1").innerHTML="You pressed the Alphabet Key" ;
+			console.log("Alphabet key");
+
+		}
+	
+	}
+	if((keyPressed >=48 && keyPressed<=57)){
+		numberkey();
+		document.getElementById("d1").innerHTML="You pressed the Number Key" ;
+		console.log("Number key");
+
+	}
+}
+if((keyPressed >=37 && keyPressed<=40)){
+	arrowkey();
+	document.getElementById("d1").innerHTML="You pressed the Arrow Key" ;
+	console.log("Arrow key");
+
+}
+
+if((keyPressed >=13 && keyPressed<=27)){
+	specialkey();
+	document.getElementById("d1").innerHTML="You pressed the Special Key" ;
+	console.log("Specialkey");
+
+}
+
+	
+	
+
+
+function aplhabetkey()
+{
+	img_image="Alphey.png";
+	add();
+
+}
+function numberkey()
+{
+	img_image="Numkey.png";
+	add();
+	
+}
+function arrowkey()
+{
+	img_image="Arrykey.png";
+	add();
+}
+function specialkey()
+{
+	img_image="spkey.png";
+	add();
+	
+}
+
+
